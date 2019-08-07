be7af78cde0e6e507caae1ed1376baa3af6a5090
Sunny <sunnyyong2@gmail.com>
2019-08-07 10:28:37 +0900

    model 선언

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=200)
+    due_date = models.DateField()
